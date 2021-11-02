e5ea0e3917ab9c155d9725b6064c7d7c2af9c924
rishusiva <rishusiva@gmail.com>
2021-11-02 16:56:39 +0530

    setup commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='GBiStat',
+      version='0.3',
+      description='Statistics, Gaussian and Binomial distributions',
+      packages=['GBiStat'],
+      author = 'Rishikesh',
+      author_email = 'rishusiva@gmail.com',
+      zip_safe=False)
